1843edae4cd27f24aed36027ee354da4f25f0d52
673317908Liyi <liy@sayingdata.com>
2020-10-16 18:04:41 +0800

    制作后台首页

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,17 +1,52 @@
 import React, { Component, Fragment } from 'react';
-import { Button } from 'antd';
+import { Layout } from 'antd';
+import {
+    MenuUnfoldOutlined,
+    MenuFoldOutlined,
+} from '@ant-design/icons';
+// 样式
+import "./index.scss"
+
+// 组件
+import Aside from "./components/Aside"
+
+const { Header, Sider, Content } = Layout;
 
 export default class Index extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
-        this.state={}
+        this.state = {
+            collapsed: false
+        }
     }
     render() {
         return (
             <Fragment>
-                <Button type="primary">按鈕</Button>
-                    首页
+                <Layout className="layout_warp">
+                    <Sider className="layout_sider" collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
+                        <Aside></Aside>
+                    </Sider>
+                    <Layout>
+                        <Header className="layout_header">
+                            {React.createElement(this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+                                className: 'trigger',
+                                onClick: this.toggle,
+                            })}
+                        </Header>
+                        <Content className="layout_content">Content</Content>
+                    </Layout>
+                </Layout>
             </Fragment>
         )
     }
+    toggle = () => {
+        this.setState({
+            collapsed: !this.state.collapsed
+        })
+    }
+    onCollapse = collapsed => {
+        this.setState({ 
+            collapsed: !this.state.collapsed
+        });
+      };
 }

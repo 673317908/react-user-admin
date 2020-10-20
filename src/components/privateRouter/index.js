8a00847f95f1bc949710f182bdf36b6876cef9da
673317908 <673317908@qq.com>
2020-10-20 22:50:20 +0800

    创建私有化组件

---
+++ src/components/privateRouter/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Route,Redirect } from "react-router-dom";
+import { getInfo } from "../../utils/storage"
+const PrivateRouter = ({ component: Component, ...rest }) => {
+    return (
+        <Route
+            {...rest}
+            render={routeProps => (
+                getInfo("user_token")?<Component {...routeProps} />:<Redirect to="/"></Redirect> 
+            )}
+        />
+    );
+}
+
+export default PrivateRouter

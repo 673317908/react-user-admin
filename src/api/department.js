ab6ffcd98fc95818a20ef4a3eccda19df1bb586e
673317908Liyi <liy@sayingdata.com>
2020-10-22 16:53:23 +0800

    部门管理请求api

---
+++ src/api/department.js
@@ -0,0 +1,28 @@
+import service from "@/utils/request"
+
+// 添加部门
+export function addDepartmentApi(data) {
+    return service.request({
+        url: "/department/add/",
+        method: "POST",
+        data
+    })
+}
+
+// 获取部门列表
+export function getDepartmentApi(data) {
+    return service.request({
+        url: "/department/list/",
+        method: "POST",
+        data
+    })
+}
+
+// 删除部门
+export function deleteDepartmentApi(data) {
+    return service.request({
+        url: "/department/delete/",
+        method: "POST",
+        data
+    })
+}

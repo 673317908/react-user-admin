41e3185a40499129f2d3e697e3600ba60499a3a9
673317908Liyi <liy@sayingdata.com>
2020-10-22 16:54:29 +0800

    实现添加部门功能

---
+++ src/views/department/Add.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { Button, Form, Input, InputNumber, Radio, message } from "antd"
+import { addDepartmentApi } from "@api/department"
+class DepartmentAdd extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            formLabel: {
+                labelCol: { span: 3 },
+                wrapperCol: { span: 20 }
+            },
+            loading: false
+        }
+    }
+    // formRef = React.createRef();
+    addDepartment = (values) => {
+        this.setState({
+            loading: true
+        })
+        addDepartmentApi(values).then(res => {
+            message.info(res.data.message)
+            this.refs.form.resetFields();
+            this.setState({
+                loading: false
+            })
+        })
+    }
+    render() {
+        return (
+            <div>
+                <Form ref="form" initialValues={{ number: 1, status: true }} {...this.state.formLabel} onFinish={this.addDepartment}>
+                    <Form.Item label="部门名称" name="name" rules={[{ required: true, message: '请输入部门名称!' }]}>
+                        <Input></Input>
+                    </Form.Item>
+                    <Form.Item label="部门人数" name="number" rules={[{ required: true, message: '请输入部门人数!' }]} >
+                        <InputNumber min={0} max={100}></InputNumber>
+                    </Form.Item>
+                    <Form.Item label="禁启用" name="status">
+                        <Radio.Group name="radiogroup">
+                            <Radio value={false}>禁用</Radio>
+                            <Radio value={true}>启用</Radio>
+                        </Radio.Group>
+                    </Form.Item>
+                    <Form.Item label="描述" name="content" rules={[{ required: true, message: '请输入部门描述!' }]}>
+                        <Input.TextArea></Input.TextArea>
+                    </Form.Item>
+                    <Form.Item label="">
+                        <Button type="primary" htmlType="submit" loading={this.state.loading}>添加</Button>
+                    </Form.Item>
+                </Form>
+            </div>
+        );
+    }
+}
+
+export default DepartmentAdd;
